65125abacf0b361c65ca4399afb20612c8f0f7dc
raoufffffff <112146388+raoufffffff@users.noreply.github.com>
2025-12-17 07:29:03 +0100

    Add files via upload

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,7 @@
 import { getProducts } from '@/lib/api';
 import ProductAndCategories from './ProductAndCategories';
 
-export default async function ProductList({ Categories, subdomain, id, mainColor, logo }) {
+export default async function ProductList({ Categories = [], subdomain, id, mainColor, logo }) {
     const products = await getProducts(subdomain, id);
     return (
         <section className="px-5" id="products">
