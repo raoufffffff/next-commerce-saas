187e22acb0d199534883ac1577d5195c9a2f0638
raoufffffff <112146388+raoufffffff@users.noreply.github.com>
2025-12-26 02:09:23 +0100

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
+import ClarityAnalytics from "@/components/Analytics/ClarityAnalytics";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,6 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
+
+        <ClarityAnalytics />
+
         {children}
       </body>
     </html>
